51b071e34cada34cb6b39b9b470f34ad7cfc960f
JeaustinSirias <JEAUSTIN.SIRIAS@ucr.ac.cr>
2020-11-07 20:27:20 -0600

    adds additional templates

--- application/models.py
+++ application/models.py
@@ -5,3 +5,7 @@ class user(models.Model):
     last_name = models.CharField(max_length=30)
     username = models.CharField(max_length=15)
     password = models.CharField(max_length=10)
+
+class appointment(models.Model):
+    date = models.DateField()
+    name = models.CharField(max_length=30)
--- application/views.py
+++ application/views.py
@@ -1,7 +1,20 @@
 import datetime
 from django.http import HttpResponse
+from django 
 
 def sample(request):
     '''Displays a default view'''
     date = datetime.datetime.utcnow()
     return HttpResponse('Vista no asignada\nHora: %s' %date)
+
+def new_appointment(request):
+    return
+
+def delete_appointment(request):
+    return
+
+def check_appointment(request):
+    return
+
+
+
